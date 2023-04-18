590f82251c19a6ad9d3951e2d75a0a16e3b281fb
Jay Koshti <jay.koshti@simformsolutions.com>
2023-04-18 09:50:47 +0530

    declared memory elements

---
+++ src/Elements/MemoryElements.ts
@@ -0,0 +1,6 @@
+const _memory_div = document.getElementById('memory')!;
+const _memoryClear = <HTMLButtonElement>document.getElementById('mc');
+const _memoryRestore = <HTMLButtonElement>document.getElementById('mr');
+const _memoryTable = <HTMLDivElement>document.getElementById('m');
+
+export {_memory_div, _memoryClear, _memoryRestore, _memoryTable}
