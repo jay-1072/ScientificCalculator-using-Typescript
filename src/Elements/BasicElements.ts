1ef24e2e582305547e2ce641ae7ca792bbdc4d72
Jay Koshti <jay.koshti@simformsolutions.com>
2023-04-18 09:49:19 +0530

    declared basic elements

---
+++ src/Elements/BasicElements.ts
@@ -0,0 +1,8 @@
+const dis = <HTMLInputElement>document.getElementById("result");
+const upper = <HTMLInputElement>document.getElementById("subtext");
+const btntxt = <HTMLAnchorElement>document.getElementById("btntxt");
+const btncheck = <HTMLDivElement>document.getElementById("btn-check");
+
+export {
+    dis, upper, btntxt, btncheck
+}
