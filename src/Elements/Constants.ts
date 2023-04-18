459f8527de81c7b21522eb3f63ccb9bc1b5b47ab
Jay Koshti <jay.koshti@simformsolutions.com>
2023-04-18 09:50:09 +0530

    declared constants

---
+++ src/Elements/Constants.ts
@@ -0,0 +1,28 @@
+const WHITE_COLOR: string = 'rgb(255, 255, 255)';
+const LIGHTORANGE_COLOR: string = 'rgb(255, 191, 190)';
+const BLUE_COLOR: string = 'rgb(0, 128, 255)';
+const MEMORY_DISABLE_COLOR: string = 'rgb(225, 225, 225)';
+const MEMORY: string = 'Memory';
+const LOG: string = 'log';
+const LN: string = 'ln';
+const YROOT: string = 'yroot';
+const SQUAREROOT: string = '2√x';
+const TENTOX: string = '10<sup>x</sup>';
+const XCUBE: string = 'x<sup>3</sup>';
+const op = ['+', '-', '*', '/', '%', '.'];
+const otherInput = ["(", ")", "Backspace"];
+const ERROR = 'Error!';
+const INFINITY = 'Infinity';
+const NAN = 'NaN';
+const Invalid = 'Invalid input'
+const Empty = '';
+const Message = 'Enter value between -1 and 1';
+const Alert = 'Cannot divide by zero';
+const mode = 'DEG';
+const degMode = 'DEG';
+const radMode = 'RAD';
+
+export {
+    WHITE_COLOR, LIGHTORANGE_COLOR, BLUE_COLOR, MEMORY_DISABLE_COLOR, MEMORY, LOG, LN, YROOT, SQUAREROOT, TENTOX, XCUBE, op, otherInput,
+    ERROR, INFINITY, NAN, Invalid, Empty, Message, Alert, mode, degMode, radMode
+}
