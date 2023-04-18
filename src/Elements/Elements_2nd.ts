7a19ff307d6b4631e1fbe295d9b0f184cff40cd9
Jay Koshti <jay.koshti@simformsolutions.com>
2023-04-18 09:52:10 +0530

    declared second functions elements

---
+++ src/Elements/Elements_2nd.ts
@@ -0,0 +1,10 @@
+const _log = <HTMLAnchorElement>document.getElementById("log");
+const _ln = <HTMLAnchorElement>document.getElementById("ln");
+const _tenpow = <HTMLAnchorElement>document.getElementById("tenpow");
+const _xtoy = <HTMLAnchorElement>document.getElementById("xtoy");
+const _root = <HTMLAnchorElement>document.getElementById("root");
+const _sqr = <HTMLAnchorElement>document.getElementById("sqr");
+
+export {
+    _log, _ln, _tenpow, _xtoy, _root, _sqr
+}
